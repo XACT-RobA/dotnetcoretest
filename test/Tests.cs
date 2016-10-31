667329e0065ca410698ce6dcbd5df7e778a87deb
Robert Anderson <robertanderson1993@gmail.com>
2016-10-31 01:01:02 +0000

    Started adding unit tests for singly lined list

---
+++ test/Tests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xunit;
+using DotNetCoreTest.SinglyLinkedList;
+
+namespace Tests
+{
+    public class TestSinglyLinkedList
+    {
+        private static ISinglyLinkedList<string> ListEmpty
+        {
+            get { return new SinglyLinkedList<string>(); }
+        }
+
+        private static ISinglyLinkedList<string> ListOneElement
+        {
+            get
+            {
+                var list = new SinglyLinkedList<string>();
+                list.Add("Rob");
+
+                return list;
+            }
+        }
+
+        [Fact]
+        public void TestZeroCount()
+        {
+            Assert.Equal(ListEmpty.Count, 0);
+        }
+
+        [Fact]
+        public void TestOneCount()
+        {
+            Assert.Equal(ListOneElement.Count, 1);
+        }
+
+        [Fact]
+        public void TestIsEmpty()
+        {
+            Assert.True(ListEmpty.IsEmpty);
+        }
+
+        [Fact]
+        public void TestNotIsEmpty()
+        {
+            Assert.False(ListOneElement.IsEmpty);
+        }
+
+        [Fact]
+        public void TestHasNodes()
+        {
+            Assert.True(ListOneElement.HasNodes);
+        }
+
+        [Fact]
+        public void TestNotHasNodes()
+        {            
+            Assert.False(ListEmpty.HasNodes);
+        }
+    }
+}
